5a004bd9ffe425133cf95bfcbae8e45080aa1089
duclm3 <62235587+duclm3@users.noreply.github.com>
2021-05-06 15:30:25 +0700

    Delete 7. Spring Data Repository directory

--- 7. Spring Data Repository/thuchanh-quanly-tinh/src/main/java/repository/IProvinceRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Province;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IProvinceRepository extends PagingAndSortingRepository<Province,Long> {
-}
