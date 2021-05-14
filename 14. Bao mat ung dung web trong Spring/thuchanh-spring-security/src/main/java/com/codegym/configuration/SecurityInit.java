87d13f4489cfd4cffc6bba1963e2a28e40f574df
Le Minh Duc <duclm3.tt>
2021-05-14 09:05:35 +0700

    add 15.Phan quyen ung dung

---
+++ 14. Bao mat ung dung web trong Spring/thuchanh-spring-security/src/main/java/com/codegym/configuration/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.codegym.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//Cấu hình này chỉ đăng ký bộ lọc springSecurityFilterChain  cho mỗi URL cho ứng dụng của bạn.
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
